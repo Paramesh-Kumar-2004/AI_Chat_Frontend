76f5dd3e60b8d39c006dfdf1cf9227f2dbf43c72
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-02-09 12:00:23 +0530

    baseurl Update

--- src/API/api.js
+++ src/API/api.js
@@ -4,13 +4,13 @@ import axios from "axios";
 
 const baseURL = import.meta.env.DEV
     ? "http://localhost:2004/api/v1"
-    : "https://ai-chat-backend-rouge.vercel.app/";
+    : "https://ai-chat-backend-rouge.vercel.app/api/v1";
 
 
 const API = axios.create({
-    // baseURL,
+    baseURL,
     // "http://localhost:2004/api/v1",
-    baseURL: "https://ai-chat-backend-rouge.vercel.app/api/v1",
+    // baseURL: "https://ai-chat-backend-rouge.vercel.app/api/v1",
     withCredentials: true
 });
 
