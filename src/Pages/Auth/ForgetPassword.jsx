1712ed426b0814d033f124da9413bf9987414b98
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-03-02 16:52:19 +0530

    Rename Endpoint  forgot-password

--- src/Pages/Auth/ForgetPassword.jsx
+++ src/Pages/Auth/ForgetPassword.jsx
@@ -16,7 +16,7 @@ const ForgetPassword = () => {
         e.preventDefault();
         try {
             setIsLoading(true);
-            const response = await API.post("/auth/forgetpassword", { email });
+            const response = await API.post("/auth/forgot-password", { email });
             toast.success("Password reset link sent to your email!", {
                 position: "top-center",
                 autoClose: 2000,
@@ -44,7 +44,7 @@ const ForgetPassword = () => {
                 alt="bg"
                 className="absolute inset-0 w-full h-full object-cover"
             /> */}
-            <ParticlesBackground/>
+            <ParticlesBackground />
 
             <div className="absolute inset-0 bg-black/10"></div>
 
